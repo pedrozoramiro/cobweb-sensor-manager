b503c061b41f6e48e98d675e75869d351aa77078
RAMIRO PEDROZO <RAMIRO PEDROZO>
2016-08-23 00:18:50 -0300

    fix page navigation

--- src/client/app/widgets/sensor/sensorPaperButton.js
+++ src/client/app/widgets/sensor/sensorPaperButton.js
@@ -5,19 +5,27 @@
         .module('app.widgets')
         .directive('sensorPaperButton', sensorPaperButton);
 
-    sensorPaperButton.$inject = ['$window'];
+    sensorPaperButton.$inject = ['$window', '$location'];
 
-    function sensorPaperButton($window) {
+    function sensorPaperButton($window, $location) {
         var directive = {
             scope: {
                 'title': '@',
                 'subtitle': '@',
                 'time': '@',
                 'icon': '@',
-                'color': '@'
+                'color': '@',
+                'path': '@'
             },
             restrict: 'E',
             templateUrl: 'app/widgets/sensor/sensorPaperButton.html',
+            link: function(scope, element, attrs) {
+
+                scope.goTo = function() {
+                    console.log(scope);
+                    $location.path(scope.path);
+                }
+            }
         };
         return directive;
     }
